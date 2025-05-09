701ef7e8af20919dccdbd3cd690bc9f3422deebc
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-09 15:21:57 +0300

    update svetparsing.py

--- divansvetpars/divansvetpars/spiders/svetparsing.py
+++ divansvetpars/divansvetpars/spiders/svetparsing.py
@@ -3,8 +3,14 @@ import scrapy
 
 class SvetparsingSpider(scrapy.Spider):
     name = "svetparsing"
-    allowed_domains = ["divan.ru"]
-    start_urls = ["https://divan.ru"]
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        lamps = response.css("div._Ud0k")
+        for lamp in lamps:
+            yield {
+                "name" : lamp.css("div.lsooF span::text").get(),
+                "price" : lamp.css("div.pY3d2 span::text").get(),
+                "url" : lamp.css("a").attrib["href"]
+            }
